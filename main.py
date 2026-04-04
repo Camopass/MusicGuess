418c93759b2c86c23aa0052b85b161627cbbe839
ThatGoblinKing <68427291+ThatGoblinKing@users.noreply.github.com>
2026-04-04 03:04:51 -0600

    Created Host, Player, and User Classes for a mockup of Networking Functionality

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import pygame
-import socket
+import sys
+from networking import Player, Host, TEST_HOST, TEST_PORT
 
+def client():
+    p = Player()
+    p.join(TEST_HOST, TEST_PORT)
+    while True:
+        print(f"Got Back: {p.send_and_recieve_test()!r}")
+
+def host():
+    print("Launching as host...")
+    h = Host()
+    h.listen()
+    while True:
+        h.manage_player_connections()
 
 def main():
     pass
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 2:
+        main()
+    elif sys.argv[1] == "client":
+        client()
+    elif sys.argv[1] == 'host':
+        host()
+    else:
+        main()
---
+++ networking.py
@@ -0,0 +1,83 @@
+import socket
+import selectors
+import types
+import pygame
+
+TEST_HOST, TEST_PORT = '127.0.0.1', 6967
+
+class User:
+
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.selector = selectors.DefaultSelector()
+    
+    def __del__(self):
+        self.socket.close()
+
+class Host(User):
+    """A User who runs the game, acts a server."""
+
+    def __init__(self, 
+                 HOST: str = TEST_HOST, 
+                 PORT: int = TEST_PORT) -> None:
+        super().__init__()
+        self.socket.bind((HOST, PORT))
+        self.clients = []
+
+    def listen(self):
+        self.socket.listen()
+        self.socket.setblocking(False)
+        self.selector.register(self.socket, selectors.EVENT_READ, data=None)
+
+    def register_accepted_sockets(self, user_socket: socket.socket):
+        connection, address = user_socket.accept()
+        connection.setblocking(False)
+        data = types.SimpleNamespace(addr=address, inb=b"", outb=b"")
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        self.selector.register(connection, events, data=data)
+    
+    def manage_player_connections(self):
+        events = self.selector.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                self.register_accepted_sockets(key.fileobj)
+            else:
+                self.handle_connection(key, mask)
+    
+    def write_to_player(self, user_socket, socket_data):
+        if socket_data.outb:
+            sent = user_socket.send(socket_data.outb)
+            socket_data.outb = socket_data.outb[sent:]
+
+    def read_from_player(self, socket_data, recieved_data):
+        socket_data.outb += recieved_data
+
+    def handle_connection(self, 
+                          key: selectors.SelectorKey, 
+                          mask):
+        user_socket = key.fileobj
+        data = key.data
+        if mask & selectors.EVENT_READ:
+            recieved_data = user_socket.recv(1024)
+            if recieved_data:
+                self.read_from_player(data, recieved_data)
+            else:
+                self.selector.unregister(user_socket)
+                user_socket.close()
+        if mask & selectors.EVENT_WRITE:
+            self.write_to_player(user_socket, data)
+                
+
+    def __del__(self):
+        self.selector.close()
+        return super().__del__()
+
+class Player(User):
+    """A User who joins a host, acts as a client"""
+
+    def join(self, HOST, PORT):
+        self.socket.connect((HOST, PORT))
+    
+    def send_and_recieve_test(self):
+        self.socket.sendall(bytes(input(), encoding='utf-8'))
+        return self.socket.recv(1024)
