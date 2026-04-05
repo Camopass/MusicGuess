beac4a9a8a1e1f67d0218a350cd5b66bda7b6c31
camop <camopass@gmail.com>
2026-04-04 19:40:31 -0600

    Refactored file names to be all lowercase but fr this time, moved rendering code to GameView classes

---
+++ gameviews/interrogation.py
@@ -0,0 +1,74 @@
+import io
+import os
+
+import pygame
+
+import musicmanager
+from gameviews.colors import Colors
+from gameviews.gameview import GameView
+from scripts.playbackcontroller import PlaybackController
+from scripts.playbutton import PlayButton
+
+
+class Interrogation(GameView):
+
+    def __init__(self):
+        self.api_key = os.getenv("API_KEY")
+
+        self.song = musicmanager.get_top_songs("s3nse1_snorlax", self.api_key)[0]
+        self.song_audio = musicmanager.download_song(self.song)
+        self.album_cover = musicmanager.get_image(self.song)
+        self.song.cover = io.BytesIO(self.album_cover)
+
+        self.album_cover = pygame.image.load(self.song.cover).convert()
+        self.album_cover = pygame.transform.scale(self.album_cover, (280, 280))
+
+        self.playback_controller = PlaybackController(self.song_audio)
+        self.playback_controller.load()
+        self.play_button = PlayButton(pygame.rect.Rect(500, 300, 75, 100), self.playback_controller)
+
+    def update(self):
+        self.play_button.update()
+
+    def render(self, screen):
+
+
+        # Begin Rendering
+        screen.fill(Colors.WHITE)
+
+        # Red Background Separator
+        pygame.draw.polygon(screen, Colors.RED, ((650, 0), (1280, 0), (1280, 720), (890, 720)))
+        pygame.draw.aaline(screen, Colors.RED, (649, 0), (889, 720))
+
+        # Who Listens to this Song?
+        pygame.draw.rect(screen, Colors.BLUE, (118, 35, 1065, 80))
+        pygame.draw.rect(screen, Colors.BLUE, (108, 25, 1065, 80))
+        pygame.draw.rect(screen, Colors.WHITE, (111, 28, 1059, 74))
+        courier_prime_60 = pygame.font.Font("./assets/CourierPrime-Bold.ttf", 60)
+        screen.blit(courier_prime_60.render("WHO LISTENS TO THIS SONG?", True, Colors.BLUE), (180, 35))
+
+        # Progress Bar
+        pygame.draw.rect(screen, Colors.BLUE, (45, 666, 1210, 35))
+        pygame.draw.rect(screen, Colors.BLUE, (35, 656, 1210, 35))
+
+        # Album Cover
+        pygame.draw.rect(screen, Colors.BLUE, (70, 180, 300, 300))
+        pygame.draw.rect(screen, Colors.BLUE, (80, 190, 300, 300))
+
+        screen.blit(self.album_cover, (80, 190, 100, 100))
+
+        # Song Information
+        space_mono_35 = pygame.font.Font("./assets/SpaceMono-Regular.ttf", 35)
+        screen.blit(space_mono_35.render(self.song.title, True, Colors.BLUE), (70, 500))
+        space_mono_25 = pygame.font.Font("./assets/SpaceMono-Regular.ttf", 25)
+        screen.blit(space_mono_25.render(self.song.artist, True, Colors.BLUE), (70, 545))
+        space_mono_15 = pygame.font.Font("./assets/SpaceMono-Regular.ttf", 20)
+        screen.blit(space_mono_15.render(f"Plays: {self.song.total_plays}", True, Colors.BLUE), (73, 580))
+
+        # Play Button
+        self.play_button.render(screen)
+
+    def unload(self):
+        self.playback_controller.unload()
+        pygame.font.quit()
+
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
+
+from gameviews.interrogation import Interrogation
 from networking import Client, Host
 from debug.networking import TEST_HOST, TEST_PORT
-import io
-import musicmanager
-import os
+import gameviews
 from dotenv import load_dotenv, find_dotenv
 import pygame
 
@@ -26,12 +26,7 @@ def host():
 
 def main():
     load_dotenv(find_dotenv())
-    api_key = os.getenv("API_KEY")
 
-    song = musicmanager.get_top_songs("s3nse1_snorlax", api_key)[0]
-    song_audio = musicmanager.download_song(song)
-    album_cover = musicmanager.get_image(song)
-    song.cover = io.BytesIO(album_cover)
 
     pygame.init()
     pygame.font.init()
@@ -39,66 +34,21 @@ def main():
     clock = pygame.time.Clock()
     running = True
 
-    white = (245, 240, 223)
-    red = (204, 55, 40)
-    blue = (14, 66, 95)
-
-    album_cover = pygame.image.load(song.cover).convert()
-    album_cover = pygame.transform.scale(album_cover, (280, 280))
-
-    playback_controller = PlaybackController(song_audio)
-    playback_controller.load()
-    play_button = PlayButton(pygame.rect.Rect(500, 300, 75, 100), playback_controller)
+    view = Interrogation()
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        play_button.update()
-
-        # Begin Rendering
-        screen.fill(white)
-
-        # Red Background Separator
-        pygame.draw.polygon(screen, red, ((650, 0), (1280, 0), (1280, 720), (890, 720)))
-        pygame.draw.aaline(screen, red, (649, 0), (889, 720))
-
-        # Who Listens to this Song?
-        pygame.draw.rect(screen, blue, (118, 35, 1065, 80))
-        pygame.draw.rect(screen, blue, (108, 25, 1065, 80))
-        pygame.draw.rect(screen, white, (111, 28, 1059, 74))
-        courier_prime_60 = pygame.font.Font("./assets/CourierPrime-Bold.ttf", 60)
-        screen.blit(courier_prime_60.render("WHO LISTENS TO THIS SONG?", True, blue), (180, 35))
-
-        # Progress Bar
-        pygame.draw.rect(screen, blue, (45, 666, 1210, 35))
-        pygame.draw.rect(screen, blue, (35, 656, 1210, 35))
-
-        # Album Cover
-        pygame.draw.rect(screen, blue, (70, 180, 300, 300))
-        pygame.draw.rect(screen, blue, (80, 190, 300, 300))
-
-        screen.blit(album_cover, (80, 190, 100, 100))
-
-        # Song Information
-        space_mono_35 = pygame.font.Font("./assets/SpaceMono-Regular.ttf", 35)
-        screen.blit(space_mono_35.render(song.title, True, blue), (70, 500))
-        space_mono_25 = pygame.font.Font("./assets/SpaceMono-Regular.ttf", 25)
-        screen.blit(space_mono_25.render(song.artist, True, blue), (70, 545))
-        space_mono_15 = pygame.font.Font("./assets/SpaceMono-Regular.ttf", 20)
-        screen.blit(space_mono_15.render(f"Plays: {song.total_plays}", True, blue), (73, 580))
-
-        # Play Button
-        play_button.render(screen)
-
+        view.update()
+        view.render(screen)
 
         pygame.display.flip()
         clock.tick(60)
 
-    playback_controller.unload()
+    view.unload()
 
-    pygame.font.quit()
     pygame.quit()
 
 
